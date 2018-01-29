af87cc2b63690492edd2c5c47845b392631243eb
Mike Wise <mwise@microsoft.com>
2018-01-29 14:51:05 -0800

    first commit

---
+++ rbm_mw.m
@@ -0,0 +1,120 @@
+% Version 1.000 
+%
+% Code provided by Geoff Hinton and Ruslan Salakhutdinov 
+%
+% Permission is granted for anyone to copy, use, modify, or distribute this
+% program and accompanying programs and documents for any purpose, provided
+% this copyright notice is retained and prominently displayed, along with
+% a note saying that the original programs are available from our
+% web page.
+% The programs and documents are distributed without any warranty, express or
+% implied.  As the programs were written for research purposes only, they have
+% not been tested to the degree that would be advisable in any important
+% application.  All use of these programs is entirely at the user's own risk.
+
+% This program trains Restricted Boltzmann Machine in which
+% visible, binary, stochastic pixels are connected to
+% hidden, binary, stochastic feature detectors using symmetrically
+% weighted connections. Learning is done with 1-step Contrastive Divergence.   
+% The program assumes that the following variables are set externally:
+% maxepoch  -- maximum number of epochs
+% numhid    -- number of hidden units 
+% batchdata -- the data that is divided into batches (numcases numdims numbatches)
+% restart   -- set to 1 if learning starts from beginning 
+
+%% Mike Notes
+%% The first time you run this you need to run the convert.m program to create
+%%     the digitN.mat filesep
+%% Afterwards you need to run makebatches.m every time before you run this 
+%%     in order to create the batchdata 3-dimentional array in memory
+%% You also probably want to enter "more off" in order to turn off paging
+
+maxepoch = 10
+numhid = 200
+restart = 1
+
+epsilonw      = 0.1;   % Learning rate for weights 
+epsilonvb     = 0.1;   % Learning rate for biases of visible units 
+epsilonhb     = 0.1;   % Learning rate for biases of hidden units 
+weightcost  = 0.0002;   
+initialmomentum  = 0.5;
+finalmomentum    = 0.9;
+
+[numcases numdims numbatches]=size(batchdata);
+
+if restart ==1,
+  restart=0;
+  epoch=1;
+
+% Initializing symmetric weights and biases. 
+  vishid     = 0.1*randn(numdims, numhid);
+  hidbiases  = zeros(1,numhid);
+  visbiases  = zeros(1,numdims);
+
+  poshidprobs = zeros(numcases,numhid);
+  neghidprobs = zeros(numcases,numhid);
+  posprods    = zeros(numdims,numhid);
+  negprods    = zeros(numdims,numhid);
+  vishidinc  = zeros(numdims,numhid);
+  hidbiasinc = zeros(1,numhid);
+  visbiasinc = zeros(1,numdims);
+  batchposhidprobs=zeros(numcases,numhid,numbatches);
+end
+
+starttrain = clock (); 
+fprintf(1,'maxepoch %d\n',maxepoch); 
+for epoch = epoch:maxepoch,
+ startepoch = clock (); 
+ fprintf(1,'epoch %d\n',epoch); 
+ errsum=0;
+ for batch = 1:numbatches,
+ if mod(batch,25)==0,
+   fprintf(1,'epoch %d batch %d\r',epoch,batch); 
+ end;
+
+%%%%%%%%% START POSITIVE PHASE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  data = batchdata(:,:,batch);
+  poshidprobs = 1./(1 + exp(-data*vishid - repmat(hidbiases,numcases,1)));    
+  batchposhidprobs(:,:,batch)=poshidprobs;
+  posprods    = data' * poshidprobs;
+  poshidact   = sum(poshidprobs);
+  posvisact = sum(data);
+
+%%%%%%%%% END OF POSITIVE PHASE  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  poshidstates = poshidprobs > rand(numcases,numhid);
+
+%%%%%%%%% START NEGATIVE PHASE  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  negdata = 1./(1 + exp(-poshidstates*vishid' - repmat(visbiases,numcases,1)));
+  neghidprobs = 1./(1 + exp(-negdata*vishid - repmat(hidbiases,numcases,1)));    
+  negprods  = negdata'*neghidprobs;
+  neghidact = sum(neghidprobs);
+  negvisact = sum(negdata); 
+
+%%%%%%%%% END OF NEGATIVE PHASE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  err= sum(sum( (data-negdata).^2 ));
+  errsum = err + errsum;
+
+   if epoch>5,
+     momentum=finalmomentum;
+   else
+     momentum=initialmomentum;
+   end;
+
+%%%%%%%%% UPDATE WEIGHTS AND BIASES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+    vishidinc = momentum*vishidinc + ...
+                epsilonw*( (posprods-negprods)/numcases - weightcost*vishid);
+    visbiasinc = momentum*visbiasinc + (epsilonvb/numcases)*(posvisact-negvisact);
+    hidbiasinc = momentum*hidbiasinc + (epsilonhb/numcases)*(poshidact-neghidact);
+
+    vishid = vishid + vishidinc;
+    visbiases = visbiases + visbiasinc;
+    hidbiases = hidbiases + hidbiasinc;
+
+%%%%%%%%%%%%%%%% END OF UPDATES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+  end
+  elap = etime (clock (), startepoch);
+  fprintf(1, 'epoch %4i error %6.1f  secs:%6.1f \n', epoch, errsum, elap); 
+end;
+totelap = etime (clock (), starttrain);
+fprintf(1, 'finalerror %6.1f  total secs:%6.1f \n', errsum, totelap); 
